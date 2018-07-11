eddf63d41ee9891e203a2f1e970b474ba4dc2f85
Joseph Smith <jmcsmith@gmail.com>
2018-07-11 11:29:10 -0400

    Initial Commit

---
+++ mtg-scraper/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  mtg-scraper
+//
+//  Created by Joseph Smith on 7/11/18.
+//  Copyright © 2018 Robotic Snail Software. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
